96f2b2d313690e874df6cf7080e1a0df111b8a02
webcrafter000 <nagendranalam1611@gmail.com>
2025-04-05 20:51:39 +0530

    fixed eventemitter

--- src/services/WalletService.ts
+++ src/services/WalletService.ts
@@ -1,4 +1,4 @@
-import EventEmitter from 'events'
+import { EventEmitter } from '../utils/EventEmitter'
 import { generateRandomNode, generateRandomTxid } from '../utils/generators'
 
 // WebLN types
